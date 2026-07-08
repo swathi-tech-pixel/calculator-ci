763bcddbb0597ebba648f5539c510e30e62408e6
swathi-tech-pixel <swathikuppala507@gmail.com>
2026-07-08 16:00:20 +0530

    Added Calculator class and JUnit tests

---
+++ src/test/java/com/swathi/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.swathi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(10, calculator.add(5, 5));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(20, calculator.multiply(4, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(5, calculator.divide(10, 2));
+    }
+}
